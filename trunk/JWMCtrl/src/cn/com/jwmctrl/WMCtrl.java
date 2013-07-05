83acdab08026029732a016ebccba5314641c65e0
zhengbowang1984@gmail.com <zhengbowang1984@gmail.com@d90528dc-2b2d-d336-a88b-b298965fb2a7>
2013-07-05 23:51:29 +0000

    # 1.0.0 beta3 * Upgrade jna-3.5.2.jar and platform-3.5.2.jar to version 4.0.0.

--- trunk/JWMCtrl/src/cn/com/jwmctrl/WMCtrl.java
+++ trunk/JWMCtrl/src/cn/com/jwmctrl/WMCtrl.java
@@ -33,7 +33,7 @@ public class WMCtrl {
 	private static Xmu xmu = null;
 
 	private static final String PROG_NAME = "jwmctrl";
-	private static final String VERSION = "1.0-beta2";
+	private static final String VERSION = "1.0-beta3";
 	private static final String HELP = "jwmctrl "
 			+ VERSION
 			+ "\n"
